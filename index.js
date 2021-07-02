50e595ed205fbcb3d25c1693f872c890738c7b25
cristina <cristinaxavierprodutora.gmail.com>
2021-07-02 02:37:40 -0300

    jsexercicio

---
+++ index.js
@@ -0,0 +1,41 @@
+let vestidos = ["floral", "branco", "preto", "amarelo", "azul"];
+console.log(vestidos);
+           
+let vestidos= ["floral", "branco", "preto", "amarelo", "azul"];
+vestidos.unshift("Bolinhas");
+console.log(vestidos);
+
+let vestidos= ["Bolinhas","floral", "branco", "preto", "amarelo", "azul"];
+ vestidos.pop("azul");
+console.log(vestidos);
+
+let vestidos= ["floral", "branco", "preto", "amarelo"];
+vestidos.push("lemon", "vermelho")
+console.log(vestidos);
+
+let vestidos =  ["floral", "branco", "preto", "amarelo", "lemon","vermelho"];
+vestidos.push("vermelho")
+console.log(vestidos);
+
+let numbers = [7,5,6,3,8,9,2,1,4]
+console.log(numbers);
+
+function myFunction1() {
+numbers.sort(function(a, b){return a - b});
+ 
+}
+ // Crie um array que receba 5 itens e exiba no console.
+
+// Utilize um método para adicionar um nome ao inicio do array.
+
+// Utilize um método para remover o último nome do array.
+
+
+// Utilize um método para adicionar dois nomes ao fim do array.
+
+
+// Utilize um método para remover o primeiro nome do array.
+
+
+// Utilize um método para organizar em ordem crescente o seguinte array:
+// const numbers = [7,5,6,3,8,9,2,1,4]
